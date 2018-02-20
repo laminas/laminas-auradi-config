8d2b46d26e6139b61527ff4a74c11cff8e5931fe
webimpress <contact@webimpress.com>
2018-02-20 08:47:48 +0000

    Invokable tests from common container test library

---
+++ test/ContainerTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-auradi-config for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-auradi-config/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace ZendTest\AuraDi\Config;
+
+use Psr\Container\ContainerInterface;
+use Zend\AuraDi\Config\Config;
+use Zend\AuraDi\Config\ContainerFactory;
+use Zend\ContainerTest\InvokableTestTrait;
+
+class ContainerTest extends \Zend\ContainerTest\ContainerTest
+{
+    use InvokableTestTrait;
+
+    protected function createContainer(array $config) : ContainerInterface
+    {
+        $factory = new ContainerFactory();
+
+        return $factory(new Config(['dependencies' => $config]));
+    }
+}
