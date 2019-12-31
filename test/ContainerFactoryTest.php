4a6a26eb09baf15900d533e4330e4a79eb1da47a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:13:20 -0600

    Rewriting as Laminas Project package

--- test/ContainerFactoryTest.php
+++ test/ContainerFactoryTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-auradi-config for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-auradi-config/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-auradi-config for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-auradi-config/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-auradi-config/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\AuraDi\Config;
+namespace LaminasTest\AuraDi\Config;
 
 use Aura\Di\ContainerConfigInterface;
+use Laminas\AuraDi\Config\ContainerFactory;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Zend\AuraDi\Config\ContainerFactory;
 
 class ContainerFactoryTest extends TestCase
 {
