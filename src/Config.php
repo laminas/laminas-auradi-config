6859db7470f31185fd47ac754211daab4e161f7d
Michał Bundyra <contact@webimpress.com>
2020-04-20 19:18:55 +0100

    Change to use is_string instead of is_int
    We want to register only string aliases
    
    - per @boesing
    
    Signed-off-by: Michał Bundyra <contact@webimpress.com>

--- src/Config.php
+++ src/Config.php
@@ -134,7 +134,7 @@ class Config implements ContainerConfigInterface
             && is_array($dependencies['invokables'])
         ) {
             foreach ($dependencies['invokables'] as $service => $class) {
-                if (! is_int($service) && $service !== $class) {
+                if (is_string($service) && $service !== $class) {
                     $container->set($service, $container->lazyGet($class));
                 }
 
@@ -156,7 +156,7 @@ class Config implements ContainerConfigInterface
             && is_array($dependencies['aliases'])
         ) {
             foreach ($dependencies['aliases'] as $alias => $target) {
-                if (is_int($alias)) {
+                if (! is_string($alias)) {
                     continue;
                 }
 
